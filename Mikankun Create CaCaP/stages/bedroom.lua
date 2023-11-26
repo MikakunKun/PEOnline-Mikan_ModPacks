9e48e7ce3a472c71dc164ea68bd99513d841928d
Mikankun <120381567+MikakunKun@users.noreply.github.com>
2023-11-26 21:54:26 +0900

    remove the folders

--- Mikankun Create CaCaP/stages/bedroom.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('bedroomback', 'bedroomback', -600, -300);
-	setScrollFactor('bedroomback', 1, 1);
-
-
-	addLuaSprite('bedroomback', false);
-end
